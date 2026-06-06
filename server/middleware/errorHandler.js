9adc41ea956bd0e807010bc511bc131bb8104ecb
vishhh443 <vishu140902@gmail.com>
2026-06-06 21:09:28 +0530

    Remove comment from errorHandler.js
    Removed comment about error handling middleware.

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -1,4 +1,4 @@
-// Error handling middleware to format responses according to requirements
+
 export const errorHandler = (err, req, res, next) => {
   console.error('[Error Handler] Caught error:', err);
   
